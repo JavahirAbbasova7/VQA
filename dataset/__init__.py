9ea3d7c2c4677ab43a48e58a21e08cf6b8d36326
Gurnoor Singh <gurnoor6@gmail.com>
2024-05-07 16:53:41 +0000

    setup code for pretraining

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -5,10 +5,21 @@ from PIL import Image
 
 from dataset.vqa_dataset import vqa_dataset
 from dataset.randaugment import RandomAugment
+from dataset.caption_dataset import pretrain_dataset
 
 def create_dataset(dataset, config):
     
     normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))   
+
+    pretrain_transform = transforms.Compose([                        
+        transforms.RandomResizedCrop(config['image_res'],scale=(0.2, 1.0), interpolation=Image.BICUBIC),
+        transforms.RandomHorizontalFlip(),
+        RandomAugment(2,7,isPIL=True,augs=['Identity','AutoContrast','Equalize','Brightness','Sharpness',
+                                            'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
+        transforms.ToTensor(),
+        normalize,
+    ])
+
     train_transform = transforms.Compose([                        
             transforms.RandomResizedCrop(config['image_res'],scale=(0.5, 1.0), interpolation=Image.BICUBIC),
             transforms.RandomHorizontalFlip(),
@@ -22,6 +33,10 @@ def create_dataset(dataset, config):
         transforms.ToTensor(),
         normalize,
         ])   
+
+    if dataset=='pretrain':
+        dataset = pretrain_dataset(config['train_file'], pretrain_transform)                  
+        return dataset  
   
     if dataset=='vqa': 
         train_dataset = vqa_dataset(config['train_file'], train_transform, config['vqa_root'], config['vg_root'], split='train') 
---
+++ models/scoring.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class ScoringModel(nn.Module):
+    def __init__(self, embed_dim):
+        super(ScoringModel, self).__init__()
+        self.score_net = nn.Sequential(
+            nn.LayerNorm(embed_dim),
+            nn.Linear(embed_dim, embed_dim),
+            nn.GELU(),
+            nn.Linear(embed_dim, embed_dim // 2),
+            nn.GELU(),
+            nn.Linear(embed_dim // 2, embed_dim // 4),
+            nn.GELU(),
+            nn.Linear(embed_dim // 4, embed_dim)
+        )
+
+    def forward(self, x):
+        return self.score_net(x)
