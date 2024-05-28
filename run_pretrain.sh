947482ed06d3b49bb6ea081b37e2c83bf5fec111
Singla17 <ssingla2018@gmail.com>
2024-05-27 23:54:43 -0700

    Small fixes

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -2,7 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=6
 
-python pretrain.py \
+python -m torch.distributed.launch --nproc_per_node=1 --use_env pretrain.py \
 --config ./config_pretrain.yaml \
 --output_dir output/pretrain/4M_caption_27May \
 --checkpoint data/ALBEF_4M.pth
